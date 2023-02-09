2a911eec1ccd61752f405b04cb2449e9a884da99
AngelinaTomashkevych <porucnikangelina@gmail.com>
2023-02-09 17:07:25 +0200

    dev-006: Init react, redax, router

---
+++ src/Components/Pages/Login/Login.jsx
@@ -0,0 +1,11 @@
+function Login() {
+  return (
+    <div>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos vero quaerat
+      assumenda ducimus. Ullam quas, autem, placeat voluptate error, alias totam
+      est debitis illum veritatis officia maiores tenetur laborum! Iste.
+    </div>
+  );
+}
+
+export default Login;
